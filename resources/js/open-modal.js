c4d4155452d3d7f38db4eef5efbbfac1f9ddfe04
Daniel Isaksson <Daniel.Isaksson@yh.Nackademin.se>
2025-04-20 18:21:00 +0200

    Modal adjustements. Added header and close button.

--- resources/js/open-modal.js
+++ resources/js/open-modal.js
@@ -1,5 +1,9 @@
 function openImageModal(imgElement) {
     const modalImg = document.getElementById('modalImage');
+    const modalTitle = document.getElementById('imageModalLabel');
+
     modalImg.src = imgElement.src;
     modalImg.alt = imgElement.alt;
+
+    modalTitle.textContent = imgElement.alt || "Image";
 }
